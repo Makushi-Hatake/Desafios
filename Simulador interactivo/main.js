18ccbca9e9b5044d38a9e27e4c9d4913ea232e3c
Makushi <maximilianosores@gmail.com>
2022-03-26 12:06:17 -0300

    array constructor

--- Simulador interactivo/main.js
+++ Simulador interactivo/main.js
@@ -3,42 +3,55 @@
 let total = 0;
 let precio = 0;
 let otroProducto = false;
-                
-function agregarAlCarrito(){
-
-    do {
-        alert("Lista de precios: Remera $800 , Pantalon $1000, Gorra $500");
-        let producto = prompt("Ingresa el número del producto a comprar: 1-Remera 2-Pantalon 3-Gorra",0); //Pertime al usuario elejir el item mediante un número.
-        let cantidad = prompt("Que cantidad quieres comprar?", 0);
-       
-        switch (producto){
-            case "1":
-                precio = 800;
-                subtotal = precio * cantidad;   //Realiza un producto entre el precio y la cantidad del item seleccionado.
-                alert("Agregaste "+ cantidad + " Remera/s de $800. Suman " + "$" + subtotal + " a tu compra."); //Informa el subtotal del item seleccionado.
-                break;
-            case "2":
-                precio = 1000;
-                subtotal = precio * cantidad;
-                alert("Agregaste "+ cantidad + " Pantalon/es de $1000. Suman " + "$" + subtotal + " a tu compra.");
-                break;
-            case "3":
-                precio = 500;
-                subtotal = precio * cantidad;
-                alert("Agregaste "+ cantidad + " Gorra/s de $500. Suman " + "$" + subtotal + " a tu compra.");      
-                break;
-            default:
-                alert("Alguno de los datos ingresados no son correctos");
-                precio = 0;
-                cantidad = 0;
-        }
-        
-        total = total + precio * cantidad;
-        alert("LLevas un total de " + "$"+ total + " hasta el momento.");
-        otroProducto = confirm ("Queres agregar otro producto?");
-    } while (otroProducto);
-    alert("El Total de tu compra es de " + "$"+total+".");
+class producto{
+    constructor (id, name, price){
+        this.id = parseFloat(id)
+        this.name = name;
+        this.price = parseInt(price);
+    }
 }
-agregarAlCarrito();
+const productos = [];
+productos.push(new producto (1,"Remera", 800));
+productos.push(new producto (2,"Pantalon", 1000));
+productos.push(new producto (3,"Gorra", 500));
+console.log(productos[0].id);
+    
+
+// function agregarAlCarrito(){
+
+//     do {
+//         alert("Lista de precios: Remera $800 , Pantalon $1000, Gorra $500");
+//         let producto = prompt("Ingresa el número del producto a comprar: 1-Remera 2-Pantalon 3-Gorra",0); //Pertime al usuario elejir el item mediante un número.
+//         let cantidad = prompt("Que cantidad quieres comprar?", 0);
+       
+//         switch (producto){
+//             case "1":
+//                 precio = 800;
+//                 subtotal = precio * cantidad;   //Realiza un producto entre el precio y la cantidad del item seleccionado.
+//                 alert("Agregaste "+ cantidad + " Remera/s de $800. Suman " + "$" + subtotal + " a tu compra."); //Informa el subtotal del item seleccionado.
+//                 break;
+//             case "2":
+//                 precio = 1000;
+//                 subtotal = precio * cantidad;
+//                 alert("Agregaste "+ cantidad + " Pantalon/es de $1000. Suman " + "$" + subtotal + " a tu compra.");
+//                 break;
+//             case "3":
+//                 precio = 500;
+//                 subtotal = precio * cantidad;
+//                 alert("Agregaste "+ cantidad + " Gorra/s de $500. Suman " + "$" + subtotal + " a tu compra.");      
+//                 break;
+//             default:
+//                 alert("Alguno de los datos ingresados no son correctos");
+//                 precio = 0;
+//                 cantidad = 0;
+//         }
+        
+//         total = total + precio * cantidad;
+//         alert("LLevas un total de " + "$"+ total + " hasta el momento.");
+//         otroProducto = confirm ("Queres agregar otro producto?");
+//     } while (otroProducto);
+//     alert("El Total de tu compra es de " + "$"+total+".");
+// }
+// agregarAlCarrito();
 
 
