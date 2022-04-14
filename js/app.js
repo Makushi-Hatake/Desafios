1076dc9f794133189de553fa5601fb0790cd7942
Makushi <maximilianosores@gmail.com>
2022-04-13 22:13:03 -0300

    cambios en css

--- js/app.js
+++ js/app.js
@@ -8,14 +8,16 @@ const mostrarProductos = (productos) =>{
         const div = document.createElement("div");
         div.classList.add("card");
         div.innerHTML += `
-                         <img src=${producto.img} class="card-img-top" alt="imgproducto"></img>
-                         <div class="card-body">
-                         <h5 class="card-title">${producto.nombre}</h5>
-                         <p class="card-text">${producto.desc}</p>
-                         <p class="card-text">${producto.talle}</p>
-                         <p class="card-text">Precio: $ ${producto.precio}</p>
-                         <a class="btn btn-primary" id=boton${producto.id}><i class="fas fa-cart-plus"></i></a>
-                         </div>
+        <div class="card" style="width: 18rem;">
+        <img src=${producto.img} class="card-img-top" alt="imgproducto"></img>
+        <div class="card-body">
+            <h5 class="card-title">${producto.nombre}</h5>
+            <p class="card-text">${producto.desc}</p>
+            <p class="card-text">${producto.talle}</p>
+            <p class="card-text">Precio: $ ${producto.precio}</p>
+            <a class="btn btn-primary" id=boton${producto.id}><i class="fas fa-cart-plus"></i></a>
+        </div>
+      </div>
                          `
         contenedorProductos.appendChild(div);
         const boton = document.getElementById(`boton${producto.id}`);
